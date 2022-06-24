8f487d94383c212c91f478ada8b844d299c9d8cd
ridwanmursal <ridwan09@hotmail.com>
2022-06-23 23:28:06 -0400

    added local storag feature

--- components/ApiSection/ApiSection.jsx
+++ components/ApiSection/ApiSection.jsx
@@ -1,7 +1,7 @@
 import Link from "../Link/Link"
 import styles from "./ApiSection.module.css"
 import {fetchUrl} from "./utils"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const ApiSection = () => {
   const [shortenedLinks, setShortenedLinks] = useState([])
@@ -9,32 +9,15 @@ const ApiSection = () => {
   const [validInput, setValidInput] = useState({isValid:true, message:""})
   console.log(shortenedLinks)
 
-  // const addLink = (response, validInput, setValidInput, shortenedLinks, setShortenedLinks) => {
-  //   console.log(response)
-  //   if (response.ok) {
-  //     if (!validInput.isValid) setValidInput({isValid:true, message:""})
-  //     const {result} = response
-  //     setShortenedLinks([
-  //       ...shortenedLinks, 
-  //       {longLink: result.original_link, shortLink: result.full_short_link}
-  //     ])
-  //   }else {
-  //     setValidInput({isValid:false, message: "Not a Valid Link"})
-  //   }
-  // }
+  // get/set links from and to local storage 
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("links"))
+    if (items && items.length > 0) {setShortenedLinks(items)}
+  }, [])
 
-  // const fetchUrl = async (validInput, setValidInput, shortenedLinks, setShortenedLinks) => {
-  //   if (input === "") {
-  //     setValidInput({isValid:false, message:"please input a link"})
-  //     //alert("please add a link")
-  //   }else {
-  //     //console.log(input, "in link")
-  //     const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${input}`)
-  //     const shortenedLink = await request.json()
-  //     addLink(shortenedLink, validInput, setValidInput, shortenedLinks, setShortenedLinks)
-  //   }
-  // }
-
+  useEffect(() => {
+    localStorage.setItem("links", JSON.stringify(shortenedLinks))
+  }, [shortenedLinks])
 
   return (
     <div className={styles["container"]}>
@@ -48,7 +31,7 @@ const ApiSection = () => {
         </div>
         
         <div>
-        <button className={styles["btn"]} onClick={() => fetchUrl(validInput, setValidInput, shortenedLinks, setShortenedLinks, input)}>Shorten it!</button>
+        <button className={styles["btn"]} onClick={() => fetchUrl(validInput, setValidInput, shortenedLinks, setShortenedLinks, input, setInput)}>Shorten it!</button>
         </div>
         
         
--- components/ApiSection/utils.js
+++ components/ApiSection/utils.js
@@ -1,5 +1,5 @@
 // Helper function called by fetch url to change the link state and handle errors
-const addLink = (response, validInput, setValidInput, shortenedLinks, setShortenedLinks) => {
+const addLink = (response, validInput, setValidInput, shortenedLinks, setShortenedLinks, setInput) => {
     console.log(response)
     if (response.ok) {
       if (!validInput.isValid) setValidInput({isValid:true, message:""})
@@ -8,13 +8,14 @@ const addLink = (response, validInput, setValidInput, shortenedLinks, setShorten
         ...shortenedLinks, 
         {longLink: result.original_link, shortLink: result.full_short_link}
       ])
+      setInput("")
     }else {
       setValidInput({isValid:false, message: "Not a Valid Link"})
     }
   }
 
 // Checks if input is empty and fetches url using the url shortener api
-const fetchUrl = async (validInput, setValidInput, shortenedLinks, setShortenedLinks, input) => {
+const fetchUrl = async (validInput, setValidInput, shortenedLinks, setShortenedLinks, input, setInput) => {
     if (input === "") {
       setValidInput({isValid:false, message:"please input a link"})
       //alert("please add a link")
@@ -22,7 +23,7 @@ const fetchUrl = async (validInput, setValidInput, shortenedLinks, setShortenedL
       //console.log(input, "in link")
       const request = await fetch(`https://api.shrtco.de/v2/shorten?url=${input}`)
       const shortenedLink = await request.json()
-      addLink(shortenedLink, validInput, setValidInput, shortenedLinks, setShortenedLinks)
+      addLink(shortenedLink, validInput, setValidInput, shortenedLinks, setShortenedLinks, setInput)
     }
 }
 
